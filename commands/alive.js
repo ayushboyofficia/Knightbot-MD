91c10a434d4b584891b9055473905add4133e11e
ayushboyofficia <160393536+ayushboyofficia@users.noreply.github.com>
2025-05-20 17:00:02 +0530

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,7 +1,7 @@
 const settings = require("../settings");
 async function aliveCommand(sock, chatId) {
     try {
-        const message = `*🤖 Knight Bot is Active!*\n\n` +
+        const message = `*🤖 TERMINAL Bot is Active!*\n\n` +
                        `*Version:* ${settings.version}\n` +
                        `*Status:* Online\n` +
                        `*Mode:* Public\n\n` +
@@ -19,7 +19,7 @@ async function aliveCommand(sock, chatId) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterName: 'TERMINALBot MD',
                     serverMessageId: -1
                 }
             }
