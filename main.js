ee8808f777cdbf8d22889215c71a144baef28bf8
ayushboyofficia <160393536+ayushboyofficia@users.noreply.github.com>
2025-05-22 18:15:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -95,8 +95,8 @@ const { addCommandReaction, handleAreactCommand } = require('./lib/reactions');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.channelLink = "https://chat.whatsapp.com/FxQEi8AAFt6KE5YVAVugwj";
+global.ytch = "AAYUSH X CODER";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
@@ -105,7 +105,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'AAYUSH BOT PG',
             serverMessageId: -1
         }
     }
