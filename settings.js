9d780dcafe70df957d0003114cc259fb36ca72a9
ayushboyofficia <160393536+ayushboyofficia@users.noreply.github.com>
2025-05-26 10:27:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'AAYUSH X SPAM',
   author: '‎',
   botName: "TURBO BOT SPAM",
-  botOwner: 'HUSAIN BHAI', // Your name
-  ownerNumber: '923467480162', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'AAYUSH BHAI', // Your name
+  ownerNumber: '917828026535', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
